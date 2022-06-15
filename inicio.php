130b7925bc05eea30ae622aee13305962edafcf0
victoria holguin <victoria.holguin9@misena.edu.co>
2022-06-15 05:59:26 -0500

    pages

---
+++ inicio.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form-login | colours JVLP</title>
+    <link rel="stylesheet" href="Inicio.css">
+</head>
+<body>
+    <div class="login-box">
+        <img class="avatar" src="./imagenes/logo.jpg" alt="logo de colours JVLP">
+        <h1>Ingrese aquì</h1>
+        <form>    
+
+            <!-- USERNAME -->
+            <Label for="Usuario">Usuario</Label>
+            <input type="text" placeholder="Ingrese Usuario">
+
+             <!-- Password -->
+             <Label for="Contraseña">Contraseña</Label>
+             <input type="text" placeholder="Ingrese Contraseña">
+
+             <input class="clic" type="submit" value="Ingresar">
+    
+
+             <a href="#"> ¿Olvido su contraseña?</a><br>
+             <a href="#"> ¿No tiene una cuenta?</a> <br>
+             <li><a href="registro.php">Regresar</a></li><br>
+            </form>
+    </div>
+
+    
+</body>
+</html>
